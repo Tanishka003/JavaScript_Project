d5c6f8019b19daf86fcfbdd0542f4f0bf80564c4
Tanishka Verma <99746797+Tanishka003@users.noreply.github.com>
2024-03-14 19:48:52 +0530

    Delete mini_project directory

--- mini_project/variable.js
+++
@@ -1,9 +0,0 @@
-const studentId = 192304608; //data
-let studentName = "Dora"
-let studentEmail = "info@dora.com"
-let studentCourse ="BCA"
-let studentCity; // get undefined bcoz data is not given
-
-console.log("Student Details"); // head of table
-
-console.table([studentId, studentName, studentEmail, studentCourse, studentCity]); //  data will print in table from
