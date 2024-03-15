b53ed59a557040721805b9673281c2a71c0d3980
Tanishka Verma <99746797+Tanishka003@users.noreply.github.com>
2024-03-15 03:29:44 +0000

    dataTypes and their conversion type

---
+++ sample_code/datatype_conversion.js
@@ -0,0 +1,21 @@
+let value = 67
+
+console.log(typeof value);
+
+let strvalue = String(value)
+
+
+console.log(strvalue); //67
+console.log(typeof strvalue); //string
+
+let boolvalue = Boolean(value)
+console.log(boolvalue); //true
+console.log(typeof boolvalue); //boolean
+
+let undefinedval = ""
+console.log(undefinedval); // empty
+console.log(typeof undefinedval); //string
+
+let nullval = null;
+console.log(nullval); // null
+console.log(typeof nullval); // object
