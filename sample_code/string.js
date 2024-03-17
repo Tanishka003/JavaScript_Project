91db0ac7d312072c166788152273b390280f11be
Tanishka Verma <99746797+Tanishka003@users.noreply.github.com>
2024-03-17 17:37:48 +0000

    String and their method

---
+++ sample_code/string.js
@@ -0,0 +1,15 @@
+const ram = "people"
+const syam = "male"
+console.log(`${ram} is include the ram and ram is comes from  ${syam} category`);
+
+const people = new String('aligaveth')
+console.log(people);
+
+console.log(people[6]);
+console.log(people.__proto__); // for prototype only
+console.log(people.length);
+console.log(people.toUpperCase);
+console.log(people.charAt(5));
+const person = "    john            "
+console.log(person.trim());
+
