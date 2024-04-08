0258565736a746e9063a8d07f126ee4517929574
Tanishka Verma <99746797+Tanishka003@users.noreply.github.com>
2024-04-08 15:20:35 +0000

    function of data and time

---
+++ sample_code/date_and_time.js
@@ -0,0 +1,11 @@
+let today_date_and_time = Date()
+console.log(today_date_and_time);
+
+let tdate = new Date()
+console.log(tdate);
+console.log(tdate.toString());
+console.log(tdate.toLocaleDateString());
+console.log(tdate.toDateString());
+console.log(tdate.getDate());
+console.log(tdate.toDateString().toUpperCase());
+console.log(tdate.toLocaleTimeString('en-IN'));
