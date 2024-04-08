1fdf80ca5bb9d6b1b5b869a4b30ba030201b1641
Tanishka Verma <99746797+Tanishka003@users.noreply.github.com>
2024-04-08 07:07:15 +0000

    number and method

---
+++ sample_code/math.js
@@ -0,0 +1,9 @@
+const value  = 145;
+const val = new Number(10245)
+console.log(`Value = ${value}\nval = ${val}`);
+
+console.log(val.toString().length);
+console.log(val.toLocaleString());
+console.log(val.toExponential);
+console.log(val.valueOf());
+console.log(val.toFixed());
