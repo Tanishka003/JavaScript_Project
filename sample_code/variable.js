77ef3e656bc4e3f477e4ff8e5bab6f7d945e0f17
Tanishka Verma <99746797+Tanishka003@users.noreply.github.com>
2024-04-10 04:11:21 +0000

    add more examples

--- sample_code/variable.js
+++ sample_code/variable.js
@@ -7,3 +7,15 @@ let studentCity; // get undefined bcoz data is not given
 console.log("Student Details"); // head of table
 
 console.table([studentId, studentName, studentEmail, studentCourse, studentCity]); //  data will print in table from
+
+
+
+var person = "frozen" 
+console.log(person);
+// var create a block  scope issue and known which assign value want to change it replace all value of same name of variable
+
+let username = "rita"
+let password = 12345
+
+console.log("LOGIN");
+console.table(`Username = ${username}\nPassword = ${password}`);
